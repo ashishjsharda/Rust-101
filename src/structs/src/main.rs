ed71e7384163816d0346fd3583e809f5bad45168
Ashish Sharda <ashishjsharda@gmail.com>
2023-11-02 07:40:14 -0400

    Structs Example

---
+++ src/structs/src/main.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+struct Person{
+    name: String,
+    age: u8,
+    likes_oranges: bool
+
+}
+fn main() {
+    let person = Person{
+        name: String::from("Adam"),
+        likes_oranges: true,
+        age: 25
+    };
+
+    println!("Person name is {}", person.name);
+    println!("Person age is {}", person.age);
+    println!("{:?}", person);
+}
---
+++ src/structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+C:\Users\ashis\RustroverProjects\rustexamples\src\structs\target\debug\structs.exe: C:\Users\ashis\RustroverProjects\rustexamples\src\structs\src\main.rs
