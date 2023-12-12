dba552919ce17e8013338e5af1b1e92076cc8880
Ashish Sharda <ashishjsharda@gmail.com>
2023-12-12 05:54:12 -0500

    attributes example

---
+++ src/attributes/target/debug/deps/attributes.d
@@ -0,0 +1,5 @@
+C:\Users\ashis\RustroverProjects\rustexamples\src\attributes\target\debug\deps\attributes.exe: src\main.rs
+
+C:\Users\ashis\RustroverProjects\rustexamples\src\attributes\target\debug\deps\attributes.d: src\main.rs
+
+src\main.rs:
